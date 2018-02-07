e4fac7d69ad13bf0fa4f2ba8104957b2c284a696
qafrankly <31970630+qafrankly@users.noreply.github.com>
2018-02-07 10:44:20 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+
+class repo1 extends Component {
+	
+  render(){
+	  
+    return (
+      <div className='FranklyDemo'>
+	      <h2 className='demoo_text'>t1: {this.props.text}</h2>
+      </div>
+    );
+	  
+  }
+}
+
+export default repo1;
